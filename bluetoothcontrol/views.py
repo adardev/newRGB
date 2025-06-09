4eeb986f022627bdc1461ff60016222524b3ac06
Cellulose2008 <celiasofiamagos2008@gmail.com>
2025-06-09 13:45:31 -0600

    Update views.py

--- bluetoothcontrol/views.py
+++ bluetoothcontrol/views.py
@@ -96,3 +96,20 @@ response = requests.post(url, json=payload, headers=headers)
 
 print("Status:", response.status_code)
 print("Respuesta:", response.text)
+
+#Code para transferir de ARDUINO a DJANGO
+
+from django.http import JsonResponse
+
+def recibir_datos(request):
+    temp = request.GET.get("temp")
+    hum_amb = request.GET.get("hum_amb")
+    hum_tierra = request.GET.get("hum_tierra")
+    foco = request.GET.get("foco")
+    ventilador = request.GET.get("ventilador")
+    bomba = request.GET.get("bomba")
+
+    # Aquí puedes guardar en base de datos, registrar, etc.
+    print(f"TEMP: {temp} | HAMB: {hum_amb} | HT: {hum_tierra} | F:{foco} V:{ventilador} B:{bomba}")
+
+    return JsonResponse({"status": "ok"})
